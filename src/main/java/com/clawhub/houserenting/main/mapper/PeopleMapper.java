5cc58c760e91214c36333121a9826dd7af9ae975
李志明 <18120177709@163.com>
2018-03-10 18:49:35 +0800

    mybaits代码生成器

---
+++ src/main/java/com/clawhub/houserenting/main/mapper/PeopleMapper.java
@@ -0,0 +1,7 @@
+package com.clawhub.houserenting.main.mapper;
+
+import com.clawhub.generator.common.IMapper;
+import com.clawhub.houserenting.main.entity.People;
+
+public interface PeopleMapper extends IMapper<People> {
+}
