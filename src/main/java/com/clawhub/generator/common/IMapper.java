ca1ebe5555829b1a2b9314a8f105431b77033ecd
李志明 <18120177709@163.com>
2019-01-21 21:09:45 +0800

    tk.mybatis 自动生成

--- src/main/java/com/clawhub/generator/common/IMapper.java
+++ src/main/java/com/clawhub/generator/common/IMapper.java
@@ -4,15 +4,10 @@ import tk.mybatis.mapper.common.Mapper;
 import tk.mybatis.mapper.common.MySqlMapper;
 
 /**
- * <Description> Mybatis通用Mapper接口<br>
+ * Mybatis通用Mapper接口
  *
- * @param <T> <br>
- * @author LiZhiming<br>
- * @version 1.0<br>
- * @taskId <br>
- * @CreateDate 2017年11月17日 <br>
+ * @param <T> the type parameter
  */
 public interface IMapper<T> extends Mapper<T>, MySqlMapper<T> {
-
 }
 
--- src/main/java/com/clawhub/houserenting/main/mapper/PeopleMapper.java
+++
@@ -1,7 +0,0 @@
-package com.clawhub.houserenting.main.mapper;
-
-import com.clawhub.generator.common.IMapper;
-import com.clawhub.houserenting.main.entity.People;
-
-public interface PeopleMapper extends IMapper<People> {
-}
